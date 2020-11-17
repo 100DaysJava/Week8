2b44d97ab1127e33b8de2a023ce848cc2b12ceb3
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-11-17 21:25:04 +0530

    Code 175 Day 53

---
+++ Pattern97.java
@@ -0,0 +1,29 @@
+package Patterns97;
+public class Pattern {
+	public static void main(String[] args) {
+	int n = 9; // rows	
+	int x = n/2+1 ;  // x = 5 -> position
+	int p = 1;  //values
+	
+	for(int i=1; i<=n ; i++) // i= 1,2,3,4,5,6,7,8,9,10<=9->f rows
+	{
+		for(int j=1; j <=n; j++) // j=1 .. 9 cols
+		{
+			if(j == x || j == n-x+1 )  // j==5||j==5
+			  System.out.print(p); // 1,2,3,4,5,4,3,2,1
+			else
+		     System.out.print(" ");
+		}
+		System.out.println();
+		if(i<=n/2) // 5 <= 4 -> f
+		{
+			p=p+1;    // p=2,3,4,5
+			x = x-1; // x=4,3,2,1
+		}
+		else // if  i = 5,6,7,8,9
+		{
+			p=p-1; // p = 4,3,2,1,0 -> value
+			x=x+1; // x = 2,3,4,5,6-> position
+		}
+	}	
+} }
