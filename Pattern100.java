297235b28c7b2b1b3455ae248e5d5e5be3cd0bdd
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-11-22 18:46:55 +0530

    Code 178 Day 54

---
+++ Pattern100.java
@@ -0,0 +1,24 @@
+package Patterns5;
+public class Pattern100 {
+	public static void main(String[] args) {
+		int n= 9;
+		int x = n/2+1; // x=5
+		for(int i = 1; i<=n ; i++) // i = 1,2.9 rows
+		{
+			for(int j=1; j<=n; j++) // j=1..9 cols
+			{
+				if (j == x || j== n-x+1)	 		
+					System.out.print((char)(x+64));
+				else
+					System.out.print(" ");			
+			}
+			System.out.println();
+			if (i<=n/2) // i =1,
+				x=x-1; // x=4
+			else // i= 5,..
+				x = x+1;
+		}
+
+	}
+
+}
